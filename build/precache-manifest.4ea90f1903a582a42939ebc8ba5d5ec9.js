a6b13f03fd50b03e4792fd6105bdf6db049834b4
proxiex <difrentecho@gmail.com>
2020-07-22 20:32:31 +0100

    deploy

---
+++ build/precache-manifest.4ea90f1903a582a42939ebc8ba5d5ec9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "290406cc6048316e08aaa6052cda561d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b163d240c256758964c4",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "fd65e6fc7bf6b7a51a22",
+    "url": "/static/css/main.85997fca.chunk.css"
+  },
+  {
+    "revision": "b163d240c256758964c4",
+    "url": "/static/js/2.df557a07.chunk.js"
+  },
+  {
+    "revision": "a6a7ad06ecdc7d1aa2fb42c9ef6ff53a",
+    "url": "/static/js/2.df557a07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd65e6fc7bf6b7a51a22",
+    "url": "/static/js/main.6a14482d.chunk.js"
+  },
+  {
+    "revision": "7717fb7c7cf4b347f32d",
+    "url": "/static/js/runtime-main.34299a8a.js"
+  }
+]);
